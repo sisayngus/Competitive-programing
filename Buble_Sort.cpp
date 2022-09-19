00813220ac1a7444f50588205f685097fa6df626
sisayngus <106074047+sisayngus@users.noreply.github.com>
2022-09-19 12:46:05 +0300

    Create Buble_Sort.cpp

---
+++ Buble_Sort.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'countSwaps' function below.
+ *
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+
+void countSwaps(vector<int> a) {
+int size=a.size();
+int count=0;
+
+
+for(int t=0;t<size;t++){
+    
+for(int i=0;i<size;i++)
+{
+    if(i+1==size)
+    continue;
+    if(a[i]>a[i+1]){
+        count++;
+    int n=a[i+1];
+    a[i+1]=a[i];
+    a[i]=n;
+    
+}
+}
+
+}
+cout<<"Array is sorted in "<<count<<" swaps."<<endl;
+cout<<"First Element: "<<a[0]<<endl;
+cout<<"Last Element: "<<a[size-1]<<endl;
+}
+
+int main()
+{
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string a_temp_temp;
+    getline(cin, a_temp_temp);
+
+    vector<string> a_temp = split(rtrim(a_temp_temp));
+
+    vector<int> a(n);
+
+    for (int i = 0; i < n; i++) {
+        int a_item = stoi(a_temp[i]);
+
+        a[i] = a_item;
+    }
+
+    countSwaps(a);
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
